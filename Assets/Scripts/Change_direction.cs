fb60ce5fcb10723d49074cf081d40a742582f4d8
LeeJunHeang <86048372+LeeJunHeang@users.noreply.github.com>
2023-01-14 14:50:23 +0900

    적 이동방향 제어 함수 생성
    현재의 타일 type으로 적 이동하는 코드 수정 예정
    Change_direction 스크립트를 이용하여 이동 방향 제어 예정

---
+++ Assets/Scripts/Change_direction.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_direction : MonoBehaviour
+{
+    int changeType = 0; // 몹의 방향을 어디로 바꾸는지 구분
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(changeType == 0) // 위쪽
+        {
+
+        }
+        else if (changeType == 1) // 아래쪽
+        {
+
+        }
+        else if (changeType == 2) // 오른쪽
+        {
+
+        }
+        else if (changeType == 3) // 왼쪽
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
